e42321310958129370ebe702b6376aa634401ad2
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-11-05 10:19:42 +0000

    devise作成

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+# 後ほど作成
+  # root to:'homes/top'
+  # root to:'homes/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
