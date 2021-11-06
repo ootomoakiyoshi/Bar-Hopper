ba83821802727a533dbf88468170ca953fe89b87
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-11-06 10:57:23 +0000

    Bar編集機能

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -19,6 +19,23 @@ def show
  @bar = Bar.find(params[:id])
 end
 
+def edit
+    @bar = Bar.find(params[:id])
+    if current_user.id != @bar.user.id
+     redirect_to bars_path
+    end
+    @user = current_user
+end
+
+def update
+  @bar = Bar.find(params[:id])
+  if @bar.update(bar_params)
+    redirect_to bar_path(@bar), notice: "You have updated book successfully."
+  else
+    render "edit"
+  end
+end
+
 def destroy
  @bar = Bar.find(params[:id])
  @bar.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
  root to: 'homes#top'
  root to: 'homes#about'
- resources :bars, only: [:new, :create, :index, :show, :destroy]
+ resources :bars, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
