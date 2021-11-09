c0341fd510c1619121fc81ffae8a6cb83d3018c0
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-11-09 07:33:10 +0000

    follow機能

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,58 @@
+class RelationshipsController < ApplicationController
+
+ def create
+     @user = User.find(params[:follow_id])
+   following = current_user.follow(@user)
+   if following.save
+     flash[:success] = 'ユーザーをフォローしました'
+     redirect_to request.referer
+   else
+     flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+     redirect_to request.referer
+   end
+end
+def destroy
+   @user = User.find(params[:follow_id])
+   following = current_user.unfollow(@user)
+   if following.destroy
+     flash[:success] = 'ユーザーのフォローを解除しました'
+     redirect_to request.referer
+   else
+     flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+     redirect_to request.referer
+   end
+end
+
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    # binding.pry
+    @users = @user.followings
+    # render 'show_following'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    # binding.pry
+    @users = @user.followers
+    # render 'show_follower'
+  end
+
+  private
+
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email)
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+ devise_for :users
  root to: 'homes#top'
  get 'home/about' => 'homes#about'
 
  resources :bars, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
 
   resource :favorites, only: [:create, :destroy]
-
   resources :comments, only: [:create, :destroy]
 end
-resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :relationships, only: [:create, :destroy]
+  get 'followings/:id' => 'relationships#followings', as: 'followings'
+  get 'followers/:id' => 'relationships#followers', as: 'followers'
+
 end
