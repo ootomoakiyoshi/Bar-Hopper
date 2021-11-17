11057a05684b23a123f0b57a98edbd81954d7e67
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-11-17 07:44:22 +0000

    インデント

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,56 +1,56 @@
 class BarsController < ApplicationController
 
-def new
-  @bar =Bar.new
-end
+ def new
+   @bar =Bar.new
+ end
 
-def create
+ def create
   @bar = Bar.new(bar_params)
   @bar.user_id = current_user.id
-   if @bar.save
-      redirect_to bars_path
-   else
-      render :new
-   end
-end
+  if @bar.save
+   redirect_to bars_path
+  else
+   render :new
+  end
+ end
 
-def index
+ def index
   @bars = Bar.page(params[:page]).reverse_order
-end
+ end
 
-def show
- @bar = Bar.find(params[:id])
- @comment = Comment.new
-end
+ def show
+  @bar = Bar.find(params[:id])
+  @comment = Comment.new
+ end
 
-def edit
-    @bar = Bar.find(params[:id])
-    if current_user.id != @bar.user.id
-     redirect_to bars_path
-    end
-    @user = current_user
-end
+ def edit
+  @bar = Bar.find(params[:id])
+  if current_user.id != @bar.user.id
+   redirect_to bars_path
+  end
+   @user = current_user
+ end
 
-def update
+ def update
   @bar = Bar.find(params[:id])
   if @bar.update(bar_params)
-    redirect_to bar_path(@bar), notice: "You have updated Bar successfully."
+   redirect_to bar_path(@bar), notice: "You have updated Bar successfully."
   else
-    render "edit"
+   render "edit"
   end
-end
+ end
 
-def destroy
- @bar = Bar.find(params[:id])
- @bar.destroy
- redirect_to bars_path
-end
+ def destroy
+  @bar = Bar.find(params[:id])
+  @bar.destroy
+  redirect_to bars_path
+ end
 
 
-private
+ private
 
-def bar_params
+ def bar_params
   params.require(:bar).permit(:bar_name, :image, :caption)
-end
+ end
 
 end
