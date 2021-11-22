982920a5123fa60e36373601ec776aad168ba469
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-11-22 10:30:55 +0000

    add

--- db/migrate/20211122080321_create_bar_images.rb
+++ db/migrate/20211122080321_create_bar_images.rb
@@ -3,8 +3,6 @@ class CreateBarImages < ActiveRecord::Migration[5.2]
     create_table :bar_images do |t|
       t.string :image_id
       t.integer :bar_id
-      t.datetime "created_at", null: false
-    　t.datetime "updated_at", null: falsera
     　t.timestamps
     end
   end
